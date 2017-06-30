26a4fd3db13c70ea394b17c3e63eb4a3ebbc8037
goldGreatChen <chenjinwei1109@gmail.com>
2017-06-30 09:12:37 +0800

    httputil修改

--- common/src/main/java/pub/chenhuang/util/http/HttpUtil.java
+++ common/src/main/java/pub/chenhuang/util/http/HttpUtil.java
@@ -1,36 +1,20 @@
 package pub.chenhuang.util.http;
 
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.CookieStore;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.ssl.SSLContextBuilder;
-import org.apache.http.ssl.TrustStrategy;
 import org.apache.http.util.EntityUtils;
-import org.apache.log4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.net.ssl.SSLContext;
-import javax.servlet.http.HttpServletRequest;
 import java.io.InputStream;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -70,12 +54,12 @@ public class HttpUtil {
                     httpPost.setHeader(s, headers.get(s));
                 }
             }
-            StringEntity entity = new StringEntity(param, "utf-8");
-            httpPost.setEntity(entity);
-            HttpResponse response2 = httpclient.execute(httpPost);
+            StringEntity postEntity = new StringEntity(param, "utf-8");
+            httpPost.setEntity(postEntity);
+            HttpResponse resp = httpclient.execute(httpPost);
             try {
-                HttpEntity entity2 = response2.getEntity();
-                string = EntityUtils.toString(entity2, "utf-8");
+                HttpEntity respEntity = resp.getEntity();
+                string = EntityUtils.toString(respEntity, "utf-8");
             } finally {
                 httpclient.getConnectionManager().shutdown();
             }
@@ -95,12 +79,12 @@ public class HttpUtil {
                     httpPost.setHeader(s, headers.get(s));
                 }
             }
-            StringEntity entity = new StringEntity(param, "utf-8");
-            httpPost.setEntity(entity);
-            HttpResponse response2 = httpclient.execute(httpPost);
+            StringEntity postEntity = new StringEntity(param, "utf-8");
+            httpPost.setEntity(postEntity);
+            HttpResponse resp = httpclient.execute(httpPost);
             try {
-                HttpEntity entity2 = response2.getEntity();
-                string = EntityUtils.toString(entity2, "utf-8");
+                HttpEntity respEntity = resp.getEntity();
+                string = EntityUtils.toString(respEntity, "utf-8");
             } finally {
                 httpclient.getConnectionManager().shutdown();
             }
